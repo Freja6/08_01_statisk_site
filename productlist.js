742a765f14c4c5aba7951bea9c6a959f0b2afc10
Freja6 <frol0004@stud.ek.dk>
2026-03-05 21:43:45 +0100

    Fixed productlist fetch order

--- productlist.js
+++ productlist.js
@@ -1,37 +1,25 @@
-let allProducts = [];
-fetch(`https://kea-alt-del.dk/t7/api/products?category=${category}`)
-  .then((res) => res.json())
-  .then((data) => {
-    allProducts = data;
-
-    showProducts(data);
-  });
+// 1. Læs category fra URL
 const params = new URLSearchParams(window.location.search);
 const category = params.get("category");
 
-// 1. Hent alle produkter
-let url = "https://kea-alt-del.dk/t7/api/products";
+// 2. Container
+const container = document.querySelector(".product_list_container");
 
-if (category) {
-  url = `https://kea-alt-del.dk/t7/api/products?category=${category}`;
-}
-
-fetch(url)
+// 3. Hent produkter fra API
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${category}`)
   .then((response) => response.json())
   .then((data) => showProducts(data));
 
-// 2. Vis produkterne
+// 4. Vis produkterne
 function showProducts(products) {
-  const container = document.querySelector(".product_list_container");
   container.innerHTML = "";
 
   products.forEach((product) => {
     container.innerHTML += `
-      <article class="product_card ${product.soldout ? "soldout" : ""}">
-
-        ${product.discount > 0 ? `<span class="discount_badge">-${product.discount}%</span>` : ""}
+      <article class="product_card">
 
         <a href="productdetails.html?id=${product.id}">
+
           <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp">
 
           <h3>${product.productdisplayname}</h3>
@@ -46,33 +34,3 @@ function showProducts(products) {
     `;
   });
 }
-
-document.getElementById("allBtn").addEventListener("click", () => {
-  showProducts(allProducts);
-});
-
-document.getElementById("saleBtn").addEventListener("click", () => {
-  const filtered = allProducts.filter((product) => product.discount > 0);
-
-  showProducts(filtered);
-});
-
-document.getElementById("stockBtn").addEventListener("click", () => {
-  const filtered = allProducts.filter((product) => !product.soldout);
-
-  showProducts(filtered);
-});
-
-document.getElementById("sortSelect").addEventListener("change", (event) => {
-  let sorted = [...allProducts];
-
-  if (event.target.value === "priceAsc") {
-    sorted.sort((a, b) => a.price - b.price);
-  }
-
-  if (event.target.value === "priceDesc") {
-    sorted.sort((a, b) => b.price - a.price);
-  }
-
-  showProducts(sorted);
-});
